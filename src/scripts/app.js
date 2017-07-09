c8004d64f6159fa840143e083c1feab53d78db1b
Jed <johon8@gmail.com>
2017-07-09 17:04:19 -0400

    development

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,25 +1,140 @@
 /**
 	app name pd-spOrgDirectory
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import {DirectoryDisplayRow, DirectoryHeaderRow, DirectoryTable} from "./components/components";
+import styles from "../styleSheets/main"; // eslint-disable-line no-unused-vars
+
 
 //class component
 class App extends React.Component{
-	render() {
+	constructor(props) {
+		super(props);
+		this.state = {
+			staffStructured: {
+				staff: {
+					default: []
+				},
+				supervisor: []
+			}
+		};
+	}
+	componentDidMount() {
+		/*
+			employee objects must have a minimum of
+			{
+				name: "Jane Doe",
+				jobTitle: "Chief of Snickers",
+				phoneNum: "222-222-2222",
+				officeNum: "222c"
+			}
+			properties in the staff object will render in a seperate section for each property
+			put in default array if no further refinment is needed
+		*/
+		let supervisors = [
+			{
+				name: "Jane Doe",
+				jobTitle: "Chief of Snickers",
+				phoneNum: "222-222-2222",
+				officeNum: "222c"
+			}
+		];
+		let staff = [
+			{
+				name: "John Doe",
+				jobTitle: "Snickerdoodle",
+				phoneNum: "222-222-2232",
+				officeNum: "222r"
+			},
+			{
+				name: "Sue Hicker",
+				jobTitle: "Data Entry",
+				phoneNum: "222-122-2222",
+				officeNum: "224g"
+			}
+		];
+		let specificStaff = [
+			{
+				name: "Willie Wonka",
+				jobTitle: "Candy Man",
+				phoneNum: "222-111-2222",
+				officeNum: "201c"
+			},
+			{
+				name: "Jill Smith",
+				jobTitle: "Typer",
+				phoneNum: "222-222-2222",
+				officeNum: "222c"
+			}
+		];
+
+		this.setState({
+			staffStructured: {
+				staff: {
+					default: staff,
+					Leads: specificStaff
+				},
+				supervisor: supervisors
+			}
+		});
+	}
+	_sorter(a, b) {
+		if (a.name.toLowerCase() < b.name.toLowerCase()) {return -1;}
+		if (a.name.toLowerCase() > b.name.toLowerCase()) {return 1;}
+		return 0;
+	}
+	_createHeader(className, text) {
 		return (
-			<div>
-			</div>
+			<DirectoryHeaderRow 
+				className={className}
+				headerText={text}
+				key={text}
+			/>
 		);
 	}
-};
+	_createRow(keyVal, empInfo) {
+		return (
+			<DirectoryDisplayRow 
+				name={empInfo.name}
+				jobTitle={empInfo.jobTitle}
+				phoneNum={empInfo.phoneNum}
+				officeNum={empInfo.officeNum}
+				key={keyVal}
+			/>
+		);
+	}
+	_createStructure() {
+		let rowContainer = [],
+			staff = this.state.staffStructured,
+			ticker = 1;
 
-//function component
-const App = () => {
-  return (
-  	<div>
-  	</div>
-  );
-};
+		if (staff.supervisor.length > 0) {
+			rowContainer.push(this._createHeader("header", "Supervisors"));
+			staff.supervisor.sort(this._sorter);
+			staff.supervisor.forEach((item) => {
+				rowContainer.push(this._createRow(ticker, item));
+				ticker++;
+			});
+		}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+		rowContainer.push(this._createHeader("header", "Staff"));
+		for (let subSection in staff.staff) {
+			if (subSection !== "default") {
+				rowContainer.push(this._createHeader("subHeader", subSection));
+			}
+			let workingOn = staff.staff[subSection];
+			workingOn.sort(this._sorter);
+			workingOn.forEach((item) => {
+				rowContainer.push(this._createRow(ticker, item));
+				ticker++;
+			});
+		}
+		return rowContainer;
+	}
+	render() {
+		return <DirectoryTable directoryRows={this._createStructure()}/>;
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/scripts/components/components.jsx
+++ src/scripts/components/components.jsx
@@ -3,3 +3,60 @@
     author: Ted
     date: 
 */
+import React from "react";
+import PropTypes from "prop-types";
+
+export const DirectoryTable = (props) => {
+  return (
+    <div id="staffDirContainer">
+        <table>
+            <thead>
+                <tr>
+                    <th colSpan="4">
+                        <h1>Staff Directory</h1>
+                    </th>
+                </tr>
+                <tr>
+                    <th>Name</th>
+                    <th>Title</th>
+                    <th>Number</th>
+                    <th>Office</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.directoryRows}
+            </tbody>
+        </table>
+    </div>
+  );
+};
+DirectoryTable.propTypes = {
+    directoryRows: PropTypes.array.isRequired
+};
+export const DirectoryHeaderRow = (props) => {
+  return (
+    <tr>
+        <td colSpan="4" className={props.className}><span>{props.headerText}</span></td>
+    </tr>
+  );
+};
+DirectoryHeaderRow.propTypes = {
+    className: PropTypes.string.isRequired,
+    headerText: PropTypes.string.isRequired
+};
+export const DirectoryDisplayRow = (props) => {
+  return (
+    <tr>
+        <td>{props.name}</td>
+        <td>{props.jobTitle}</td>
+        <td>{props.phoneNum}</td>
+        <td>{props.officeNum}</td>
+    </tr>
+  );
+};
+DirectoryDisplayRow.propTypes = {
+    name: PropTypes.string.isRequired,
+    jobTitle: PropTypes.string.isRequired,
+    phoneNum: PropTypes.string.isRequired,
+    officeNum: PropTypes.string.isRequired
+};
